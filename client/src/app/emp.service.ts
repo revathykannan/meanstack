6b97bc46eabc7f532f14b9617362ae18ab5a669d
ramyavadlamani <vramyagnk@gmail.com>
2017-12-04 16:39:52 +0530

    deployment

--- client/src/app/emp.service.ts
+++ client/src/app/emp.service.ts
@@ -11,19 +11,19 @@ export class EmpService {
        .map(res => res.json());
   }
    addEmployee(info) {
-    return this.http.post('mongodb://venkat:venkat@ds129166.mlab.com:29166/employees', info)
+    return this.http.post('http://venkat:venkat@ds129166.mlab.com:29166/employees', info)
         .map(res => res.json());
   }
   getEmployee(id) {
-    return this.http.get('mongodb://venkat:venkat@ds129166.mlab.com:29166/employees' + id)
+    return this.http.get('http://venkat:venkat@ds129166.mlab.com:29166/employees' + id)
         .map(res => res.json());
   }
   deleteEmployee(id) {
-    return this.http.delete('mongodb://venkat:venkat@ds129166.mlab.com:29166/employees' + id)
+    return this.http.delete('http://venkat:venkat@ds129166.mlab.com:29166/employees' + id)
         .map(res => res.json());
   }
   updateEmployee(id, info) {
-    return this.http.put('mongodb://venkat:venkat@ds129166.mlab.com:29166/employees' + id, info)
+    return this.http.put('http://venkat:venkat@ds129166.mlab.com:29166/employees' + id, info)
         .map(res => res.json());
   }
 }
--- config/database.js
+++ config/database.js
@@ -3,8 +3,8 @@ const crypto = require('crypto').randomBytes(256).toString('hex'); // Provides c
 // Export config object
 module.exports = {
   uri: process.env.databaseUri, // Databse URI and database name
- //uri:'mongodb://venkat:venkat@ds129166.mlab.com:29166/employees',
- uri:'mongodb://loukya:loukya@ds259855.mlab.com:59855/meandemoemp',
+ uri:'mongodb://venkat:venkat@ds129166.mlab.com:29166/employees',
+// uri:'mongodb://loukya:loukya@ds259855.mlab.com:59855/meandemoemp',
   //uri:newFunction(),
  //var config = "mongodb://localhost:27017/employees";
   secret: crypto, // Cryto-created secret
