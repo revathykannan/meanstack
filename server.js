88a8386a1b3a45e8f158999b949cd954be92e973
revathykannan <revathy.kannan26@gmail.com>
2017-12-09 12:54:21 +0530

    first commit

--- server.js
+++ server.js
@@ -2,31 +2,36 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var cors = require('cors');
-const config = require('./config/database');
+//const config = require('./config/database');
 mongoose.Promise = global.Promise;
 var path = require('path');
+var url = require('url');
+const routes = require('./routes');
+const model = require('./model');
+var url = "mongodb://revathy:admin123@ds133746.mlab.com:33746/meanemp";
+
 //var config = "mongodb://localhost:27017/employees";
-mongoose.connect(config.uri, {
-    useMongoClient: true,
-  }, (err) => {
+//mongoose.connect(config.uri, {
+ //   useMongoClient: true,
+//  }, (err) => {
     // Check if database was able to connect
-    if (err) {
-      console.log('Could NOT connect to database: ', err); // Return error message
-    } else {
-      console.log('Connected to ' + config.db); // Return success message
-    }
-  });
-//mongoose.connect(config)
-    //.connection
-        //.on('connected',function(){
-           // console.log("successfully connected to "+ config)
-       // })
-       // .on('error',function(err){
-           // console.log("database error "+ err)
-        //})
+ //   if (err) {
+ //     console.log('Could NOT connect to database: ', err); // Return error message
+  //  } else {
+  //    console.log('Connected to ' + config.db); // Return success message
+  //  }
+ // });
+mongoose.connect(url)
+    .connection
+        .on('connected',function(){
+            console.log("successfully connected to "+ url)
+        })
+        .on('error',function(err){
+            console.log("database error "+ err)
+        })
 var app = express();
-const port = process.env.PORT || 3000;
-//var port = 3000 ;
+//const port = process.env.PORT || 3000;
+var port = 3000 ;
 
 var router = require('./routes');
 //middleware 
@@ -35,9 +40,8 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, './public')));
 app.use(cors());
 app.use('/api/employees', router);
+app.listen(process.env.PORT || 3000);
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname +'/public/index.html'));
   });
-app.listen(port, function(){
-    console.log("server is running on port  "+ port);
-})
+
